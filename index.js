079f9ae8be67c2398878f20defd84f491ab9e173
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-18 19:30:59 +0545

    though completed but don't know what went wrong horribly

--- index.js
+++ index.js
@@ -1,47 +1,121 @@
 function App() {
-    function decreaseBreak() {
 
+    const [breakValue, setBreakValue] = React.useState(5);
+    const [sessionValue, setSessionValue] = React.useState(25);
+    const [isTimerRunning, setIsTimerRunning] = React.useState(false);
+    const [timingType, setTimingType] = React.useState("SESSION");
+    const [timeLeft, setTimeLeft] = React.useState(1500);
+
+    const timeout = setTimeout(() => {
+        if (timeLeft && isTimerRunning) {
+            setTimeLeft(timeLeft - 1)
+        }
+    }, 1000);
+
+    const timeFormatter = () => {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft - minutes * 60;
+        const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+        const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+        return `${formattedMinutes}:${formattedSeconds}`;
+    }
+
+    function decreaseBreak() {
+        if (breakValue > 1) {
+            setBreakValue(breakValue - 1);
+        }
     }
     function increaseBreak() {
-
+        if (breakValue < 60) {
+            setBreakValue(breakValue + 1);
+        }
     }
     function decreaseSession() {
-
+        if (sessionValue > 1) {
+            setSessionValue(sessionValue - 1)
+        }
     }
     function increaseSession() {
-
+        if (sessionValue < 60) {
+            setSessionValue(sessionValue + 1)
+        }
     }
     function handleStart() {
-
+        clearTimeout(timeout);
+        setIsTimerRunning(!isTimerRunning);
     }
+
     function handleReset() {
-
+        clearTimeout(timeout);
+        setIsTimerRunning(false);
+        setTimeLeft(1500);
+        setBreakValue(5);
+        setSessionValue(25);
+        setTimingType("SESSION");
+        const audio = document.getElementById("beep");
+        audio.pause()
+        audio.currentTime = 0;
     }
+
+    const resetTimer = () => {
+        const audio = document.getElementById("beep");
+        if (!timeLeft && timingType === "SESSION") {
+            setTimeLeft(breakLength * 60)
+            setTimingType("BREAK")
+            audio.play()
+        }
+        if (!timeLeft && timingType === "BREAK") {
+            setTimeLeft(sessionLength * 60)
+            setTimingType("SESSION")
+            audio.pause()
+            audio.currentTime = 0;
+        }
+    }
+
+    const clock = () => {
+        if (isTimerRunning) {
+            timeout
+            resetTimer();
+        } else {
+            clearTimeout(timeout);
+        }
+    }
+
+    React.useEffect(() => {
+        clock()
+    }, [isTimerRunning, timeLeft, timeout])
+
     return (
         <>
             <header>25 + 5 Clock</header>
             <div className="holder">
                 <h2 id="break-label">break length</h2>
                 <div className="flexbox">
-                    <button onClick={decreaseBreak} id="break-decrement">⬇</button>
-                    <div id="break-length">5</div>
-                    <button onClick={increaseBreak} id="break-increment">⬆</button>
+                    <button disabled={isTimerRunning} onClick={decreaseBreak} id="break-decrement">⬇</button>
+                    <div id="break-length">{breakValue}</div>
+                    <button disabled={isTimerRunning} onClick={increaseBreak} id="break-increment">⬆</button>
                 </div>
             </div>
             <div className="holder">
                 <h2 id="session-label">session length</h2>
                 <div className="flexbox">
-                    <button onClick={decreaseSession} id="session-decrement">⬇</button>
-                    <div id="session-length">25</div>
-                    <button onClick={increaseSession} id="session-increment">⬆</button>
+                    <button disabled={isTimerRunning} onClick={decreaseSession} id="session-decrement">⬇</button>
+                    <div id="session-length">{sessionValue}</div>
+                    <button disabled={isTimerRunning} onClick={increaseSession} id="session-increment">⬆</button>
                 </div>
             </div>
             <div className="holder">
-                <h2 id="timer-label">Session</h2>
-                <div id="time-left">25:00</div>
+                <h2 id="timer-label">{timingType == "SESSION" ? "Session" : "Break"}</h2>
+                <div id="time-left">{timeFormatter()}</div>
             </div>
             <button id="start_stop" onClick={handleStart}>start/stop</button>
             <button id="reset" onClick={handleReset}>reset</button>
+            <audio
+                id="beep"
+                preload="auto"
+                src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+            />
+            <footer>created with ♥ by Nirajan</footer>
         </>
     );
 }
