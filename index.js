9d04460d83047556749e04043283d76944d98419
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-18 17:53:30 +0545

    minimal styling

--- index.js
+++ index.js
@@ -19,18 +19,29 @@ function App() {
     }
     return (
         <>
-            <div id="break-label">break length</div>
-            <div id="session-label">session length</div>
-            <button onClick={decreaseBreak} id="break-decrement">B.D.</button>
-            <button onClick={decreaseSession} id="session-decrement">S.D.</button>
-            <button onClick={increaseBreak} id="break-increment">B.I.</button>
-            <button onClick={increaseSession} id="session-increment">S.I.</button>
-            <div id="break-length">5</div>
-            <div id="session-length">25</div>
-            <div id="timer-label">Session</div>
-            <div id="time-left">25:00</div>
+            <header>25 + 5 Clock</header>
+            <div className="holder">
+                <h2 id="break-label">break length</h2>
+                <div className="flexbox">
+                    <button onClick={decreaseBreak} id="break-decrement">⬇</button>
+                    <div id="break-length">5</div>
+                    <button onClick={increaseBreak} id="break-increment">⬆</button>
+                </div>
+            </div>
+            <div className="holder">
+                <h2 id="session-label">session length</h2>
+                <div className="flexbox">
+                    <button onClick={decreaseSession} id="session-decrement">⬇</button>
+                    <div id="session-length">25</div>
+                    <button onClick={increaseSession} id="session-increment">⬆</button>
+                </div>
+            </div>
+            <div className="holder">
+                <h2 id="timer-label">Session</h2>
+                <div id="time-left">25:00</div>
+            </div>
             <button id="start_stop" onClick={handleStart}>start/stop</button>
-            <button id="reset" onClick={handleReset}></button>
+            <button id="reset" onClick={handleReset}>reset</button>
         </>
     );
 }
