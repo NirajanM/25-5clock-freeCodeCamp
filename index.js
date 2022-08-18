580c253632eef6516219d8438dda780eaf0a3ad4
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-18 17:36:40 +0545

    completed #5

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function App() {
+    function decreaseBreak() {
+
+    }
+    function increaseBreak() {
+
+    }
+    function decreaseSession() {
+
+    }
+    function increaseSession() {
+
+    }
     return (
         <>
-            <h1>hello world</h1>
+            <div id="break-label">break length</div>
+            <div id="session-label">session length</div>
+            <button onClick={decreaseBreak} id="break-decrement">B.D.</button>
+            <button onClick={decreaseSession} id="session-decrement">S.D.</button>
+            <button onClick={increaseBreak} id="break-increment">B.I.</button>
+            <button onClick={increaseSession} id="session-increment">S.I.</button>
         </>
     );
 }
